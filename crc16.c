7655ac0a54c33d762b72c923da341140b0edfab3
Onur Ünal <74546805+onurunaall@users.noreply.github.com>
2021-08-12 12:22:41 +0300

    Create crc16.c

---
+++ crc16.c
@@ -0,0 +1,25 @@
+unsigned int CalculateCRC16(char* message, unsigned short length) {
+	int crc = 0;
+	int j = 0;
+	const int poly = 0x1021;
+	if (length == 0) {
+		return crc;
+	}
+	if (message == null)
+		return 0;
+	while (length != 0) {
+		crc ^= (message[j] << 8); //converting 8-bits of data to 16-bits of data and intermedşat crc value
+		j += 1;
+		for (int i = 0; i < 8; i++) {
+			if ((crc & (0x****)) != 0) {
+				crc = crc << 1;
+				crc ^= poly;
+			}
+			else {
+				crc = crc << 1;
+			}
+		}
+		length -= 1;
+	}
+	crc &= (0xFFFF);
+}
